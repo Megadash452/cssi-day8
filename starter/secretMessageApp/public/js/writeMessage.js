ae8f9e63b17d479085aff212ae3251a9bfd7b03d
Martin Molina <marticraft680@gmail.com>
2021-07-23 05:51:51 +0000

    day 9 medium

--- starter/secretMessageApp/public/js/writeMessage.js
+++ starter/secretMessageApp/public/js/writeMessage.js
@@ -8,16 +8,23 @@ function sendMsg(msg, pass) {
     }
 }
 
-let textarea = document.getElementById('message');
-let submitBtn = document.getElementById('submit');
-let textCount = document.getElementById('text-count');
+const textarea = document.getElementById('message');
+const submitBtn = document.getElementById('submit');
+const textCount = document.getElementById('text-count');
+const passElement = document.getElementById('passcode');
+
+let passOk = false;
+
+const symbols = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
 
 submitBtn.addEventListener('click', e => {
-    sendMsg(
-        textarea.value,
-        document.getElementById('passcode').value
-    );
-    textarea.value = "";
+    if (passOk) {
+        sendMsg(
+            textarea.value,
+            passElement.value
+        );
+        textarea.value = "";
+    }
 });
 
 textarea.addEventListener('input', e => {
@@ -29,4 +36,51 @@ textarea.addEventListener('input', e => {
     else {
         textCount.style.color = "#4a4a4a";
     }
+});
+
+passElement.addEventListener('change', e => {
+    let upper  = false,
+        num    = false,
+        symbol = false;
+    const upperElement  = document.querySelector('div.control strong.upper'),
+          numElement    = document.querySelector('div.control strong.num'),
+          symbolElement = document.querySelector('div.control strong.symbol');
+    console.log(upper, num, symbol);
+    let pass = passElement.value;
+
+    
+    for (let i=0; i < pass.length; i++) {
+        let char = pass[i];
+        // check 1 uppercase
+        if (char === char.toUpperCase() && char != char.toLowerCase())
+            upper = true;
+        // check 1 number
+        if (char == parseInt(char))
+            num = true;
+        // check 1 symbol
+        if (symbols.test(char)) {
+            symbol = true;
+        }
+    }
+
+    if (upper && num && symbol) {
+        passOk = true
+        passElement.style.borderColor = '#dbdbdb'
+    } else {
+        passOk = false;
+        passElement.style.borderColor = 'red';
+    }
+    
+    if (upper)
+        upperElement.innerText = '1 Uppercase ✔';
+    else
+        upperElement.innerText = '1 Uppercase ❌';
+    if (num)
+        numElement.innerText = '1 number ✔';
+    else
+        numElement.innerText = '1 number ❌';
+    if (symbol)
+        symbolElement.innerText = '1 symbol ✔';
+    else
+        symbolElement.innerText = '1 symbol ❌';
 });
